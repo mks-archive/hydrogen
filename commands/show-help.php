4edbcf615597ebd21084d4dc425e86e94d4bae62
Mike Schinkel <mike@newclarity.net>
2012-06-03 19:09:49 -0400

    Added LICENSE, README, samples, ignored .gitignore and related.

--- commands/show-help.php
+++ commands/show-help.php
@@ -3,6 +3,7 @@ class Hydrogen_Show_Help_Command extends Hydrogen_Command implements Hydrogen_Co
 	function run( $args, $switches ) {
 		$help =<<<HELP
 This is the help file for Hydrogen.
+Clearly we've got some work to do here.
 HELP;
 		echo $help;
 	}
