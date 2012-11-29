56e307fa132aecc60c71df7743dc9a46eed97a94
Mike Schinkel <mikeschinkel@gmail.com>
2012-11-28 23:59:53 -0500

    Added comments containing links to the CPanel API

--- commands/deploy-package.php
+++ commands/deploy-package.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * http://docs.cpanel.net/twiki/bin/view/SoftwareDevelopmentKit/ApiRef/MysqlRef
+ * http://docs.cpanel.net/twiki/bin/view/SoftwareDevelopmentKit/ApiMysql
+ * http://docs.cpanel.net/twiki/bin/view/SoftwareDevelopmentKit/ApiFileman#Fileman::fileop
+ */
 class Hydrogen_Deploy_Package_Command extends Hydrogen_Package_Command implements Hydrogen_Command_Interface {
 
 	function deploy( $package_name, $args = array() ) {
